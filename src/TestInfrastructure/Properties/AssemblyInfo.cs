1679f28f33ab457181363c1cccaca37ec2f7fc28
Richard Spence <rispenc@microsoft.com>
2018-01-18 13:37:21 -0800

    Added vs solution and nuget

--- src/TestInfrastructure/Properties/AssemblyInfo.cs
+++ src/TestInfrastructure/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("TestInfrastructure")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Unit test extensions for Unity and Moq")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Richard Spence")]
 [assembly: AssemblyProduct("TestInfrastructure")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
